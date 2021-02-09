6f5607c71f00c08d423aa3cd4a0acbc51cca6b29
Johan Kotlinski <kotlinski@gmail.com>
2021-02-09 01:21:19 +0100

    sample rule seems to work

---
+++ location.h
@@ -0,0 +1,7 @@
+#pragma once
+
+struct Location {
+    Location() : bank(0), ptr(0) {}
+    int bank;
+    int ptr;
+};
--- writer.h
+++ writer.h
@@ -6,20 +6,14 @@
 #include <map>
 #include <vector>
 
+#include "location.h"
 #include "rule.h"
-
 #include "rule_redundant_write.h"
 #include "rule_wait.h"
 #include "rule_envelope.h"
 #include "rule_pitch.h"
 #include "rule_lyc.h"
-
-// #define RECORD_WRITES
-
-#ifdef RECORD_WRITES
-FILE* music_file = fopen("music", "wb");
-FILE* sample_file = fopen("samples", "wb");
-#endif
+#include "rule_sample.h"
 
 class Writer {
     public:
@@ -29,9 +23,6 @@ class Writer {
             memset(regs, -1, sizeof(regs));
             f = 0;
             gbs_mode = false;
-            sample_count = 0;
-            curr_sample_bank = -1;
-            curr_sample_bank = -1;
             last_music_size = 0;
         }
 
@@ -50,14 +41,6 @@ class Writer {
             }
             memset(regs, -1, sizeof(regs));
             music_stream.push_back(SONG_START);
-        }
-
-        void log_written_bytes() {
-            printf("wrote %i sample bytes, %i music bytes\n",
-                    sample_count,
-                    (int)music_stream.size() - last_music_size);
-            sample_count = 0;
-            last_music_size = music_stream.size();
         }
 
         void record_song_stop() {
@@ -124,6 +107,8 @@ class Writer {
             PitchRule pitch;
             LycRule lyc;
 
+            optimize_rule(sample_rule);
+
             optimize_rule(pan);
             optimize_rule(pu0_sweep);
             optimize_rule(pu0_length);
@@ -137,36 +122,25 @@ class Writer {
             optimize_rule(envelope);
             optimize_rule(pitch);
             optimize_rule(lyc);
-
-            /*
-            if (sample_buffer_full()) {
-                sample_buffer.pop_front();
-            }
-            sample_buffer.push_back(byte);
-
-            if (sample_buffer_has_sample()) {
-                write_sample_buffer();
-            } else {
-            }
-            */
         }
 
         void write_music_to_disk() {
             optimize_music_stream();
 
+            write_samples();
+
             size_t song_start = 0;
             size_t i = 0;
+            int song_count = 0;
             while (i < music_stream.size()) {
                 if (music_stream[i] == SONG_START) {
+                    fprintf(f, "; === Start song %i\n", ++song_count);
                     song_locations.push_back(write_location);
                     song_start = i;
                 } else {
                     write_byte(music_stream[i]);
-#ifdef RECORD_WRITES
-                    fwrite(&music_stream[i], 1, 1, music_file);
-#endif
                     if (music_stream[i] == (STOP | CMD_FLAG)) {
-                        printf("Wrote %i bytes\n", (int)(i - song_start));
+                        printf("Song %i: %i bytes\n", song_count, (int)(i - song_start));
                     }
                 }
                 ++i;
@@ -179,28 +153,16 @@ class Writer {
 
         const bool gbs_mode;
 
-        struct Location {
-            Location() : bank(0), ptr(0) {}
-            int bank;
-            int ptr;
-        };
         Location write_location;
 
         unsigned int regs[0x100];
 
-        typedef std::map<std::vector<unsigned char>, Location> SampleLocations;
-        SampleLocations sample_locations;
-
-        std::deque<unsigned int> sample_buffer;
-
         std::vector<Location> song_locations;
         std::vector<unsigned int> music_stream;
 
-        int sample_count;
-        int curr_sample_bank;
-        int curr_sample_address;
-
         int last_music_size;
+
+        SampleRule sample_rule;
 
         // -----
 
@@ -342,103 +304,26 @@ class Writer {
             assert(write_location.ptr < 0x8000);
         }
 
-        bool sample_buffer_full() {
-            return sample_buffer.size() == 44;
-        }
-
-        bool sample_buffer_has_sample() {
-            /* wav write:
-             * $25=?
-             * $1a=0
-             * $30-3F=?
-             * $1a=$80
-             * $1e=?
-             * $1d=?
-             * $25=?  */
-            return sample_buffer_full() &&
-                sample_buffer[0] == (0x25 | CMD_FLAG) &&
-                sample_buffer[2] == (0x1a | CMD_FLAG) &&
-                sample_buffer[3] == 0 &&
-                sample_buffer[4] == (0x30 | CMD_FLAG) &&
-                sample_buffer[6] == (0x31 | CMD_FLAG) &&
-                sample_buffer[8] == (0x32 | CMD_FLAG) &&
-                sample_buffer[10] == (0x33 | CMD_FLAG) &&
-                sample_buffer[12] == (0x34 | CMD_FLAG) &&
-                sample_buffer[14] == (0x35 | CMD_FLAG) &&
-                sample_buffer[16] == (0x36 | CMD_FLAG) &&
-                sample_buffer[18] == (0x37 | CMD_FLAG) &&
-                sample_buffer[20] == (0x38 | CMD_FLAG) &&
-                sample_buffer[22] == (0x39 | CMD_FLAG) &&
-                sample_buffer[24] == (0x3a | CMD_FLAG) &&
-                sample_buffer[26] == (0x3b | CMD_FLAG) &&
-                sample_buffer[28] == (0x3c | CMD_FLAG) &&
-                sample_buffer[30] == (0x3d | CMD_FLAG) &&
-                sample_buffer[32] == (0x3e | CMD_FLAG) &&
-                sample_buffer[34] == (0x3f | CMD_FLAG) &&
-                sample_buffer[36] == (0x1a | CMD_FLAG) &&
-                sample_buffer[37] == 0x80 &&
-                sample_buffer[38] == (0x1e | CMD_FLAG) &&
-                sample_buffer[40] == (0x1d | CMD_FLAG) &&
-                sample_buffer[42] == (0x25 | CMD_FLAG);
-        }
-
-        void write_sample_buffer() {
-            std::vector<unsigned char> sample_contents;
-            for (int i = 5; i < 5 + 32; i += 2) {
-                sample_contents.push_back(sample_buffer[i]);
-            }
-            SampleLocations::iterator sample_location = sample_locations.find(sample_contents);
-            if (sample_location == sample_locations.end()) {
-                sample_location = sample_locations.insert(std::make_pair(sample_contents, write_location)).first;
-                for (size_t i = 0; i < sample_contents.size(); ++i) {
-                    write_byte(sample_contents[i]);
-                    ++sample_count;
-#ifdef RECORD_WRITES
-                    fwrite(&sample_contents[i], 1, 1, sample_file);
-#endif
-                }
-            }
-            unsigned int new_pitch_lsb = sample_buffer[39];
-            unsigned int new_pitch_msb = sample_buffer[41];
-            sample_buffer.clear();
-
-            if (new_pitch_lsb == regs[0x1d] && new_pitch_msb == regs[0x1e] &&
-                    curr_sample_bank == sample_location->second.bank &&
-                    curr_sample_address == sample_location->second.ptr - 0x10) {
-                sample_buffer.push_back(SAMPLE_NEXT | CMD_FLAG);
-            } else if (curr_sample_bank == sample_location->second.bank &&
-                    curr_sample_address == sample_location->second.ptr) {
-                if (regs[0x1d] != new_pitch_lsb) {
-                    sample_buffer.push_back(0x1d | CMD_FLAG);
-                    sample_buffer.push_back(new_pitch_lsb);
-                }
-                sample_buffer.push_back(0x1e | CMD_FLAG);
-                sample_buffer.push_back(new_pitch_msb);
-            } else {
-                sample_buffer.push_back(SAMPLE | CMD_FLAG);
-                assert(sample_location->second.bank < 0x100);
-                sample_buffer.push_back(sample_location->second.bank);
-                sample_buffer.push_back(sample_location->second.ptr & 0xff);
-                sample_buffer.push_back(sample_location->second.ptr >> 8);
-                sample_buffer.push_back(new_pitch_lsb);
-                sample_buffer.push_back(new_pitch_msb);
-            }
-
-            regs[0x1d] = new_pitch_lsb;
-            regs[0x1e] = new_pitch_msb;
-            curr_sample_bank = sample_location->second.bank;
-            curr_sample_address = sample_location->second.ptr;
-        }
-
-        void flush_sample_buffer() {
-            for (size_t i = 0; i < sample_buffer.size(); ++i) {
-                music_stream.push_back(sample_buffer[i]);
-            }
-            sample_buffer.clear();
-        }
-
         void record_byte(unsigned int byte) {
             music_stream.push_back(byte);
         }
 
+        void write_samples() {
+            std::vector<unsigned char> samples = sample_rule.get_samples();
+            for (size_t i = 0; i < samples.size(); i += 0x10) {
+                fprintf(f, "DB ");
+                for (size_t j = i; j < i + 0x10; ++j) {
+                    fprintf(f, "$%x", samples[j]);
+                    if ((j & 0xf) != 0xf) {
+                        fprintf(f, ",");
+                    }
+                }
+                fputs("\n", f);
+                write_location.ptr += 0x10;
+                if (write_location.ptr == 0x8000) {
+                    new_bank();
+                }
+            }
+            printf("Wrote %i samples\n", (int)samples.size());
+        }
 };
